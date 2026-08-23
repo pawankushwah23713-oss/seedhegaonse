504cb85e5734ee3d2e556b3f851a6aeba0e92c95
pawan kushwah <pawankushwah23713@gmail.com>
2026-08-23 17:36:28 +0530

    hhhh

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -36,7 +36,7 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB Limit
+  limits: { fileSize: 5 * 1024 * 1024 }, 
   fileFilter: fileFilter
 });
 
